df8826f140cc16b1b5fabdaa3d10ffe28b68a3c0
Vincenzo <v.santoro21@studenti.unisa.it>
2018-06-25 18:26:21 +0200

    Update benchmark.sh

--- projects/problems_templates/pi/src/benchmark.sh
+++ projects/problems_templates/pi/src/benchmark.sh
@@ -23,26 +23,26 @@ echo "Strong Scaling" >> benchmark.dat
 #	mpirun -np "$((i*2))" --hostfile "myhostfile_$i" Pi 1073741823 >> benchmark.dat
 #done
 
-mpirun -np 1  --hostfile myhostfile_1 Pi 2147483647 >> benchmark.dat
-mpirun -np 2  --hostfile myhostfile_2 Pi 2147483647 >> benchmark.dat
-mpirun -np 3  --hostfile myhostfile_3 Pi 2147483647 >> benchmark.dat
-mpirun -np 4  --hostfile myhostfile_4 Pi 2147483647 >> benchmark.dat
-mpirun -np 5 --hostfile myhostfile_5 Pi 2147483647  >> benchmark.dat
-mpirun -np 6 --hostfile myhostfile_6 Pi 2147483647  >> benchmark.dat
-mpirun -np 7 --hostfile myhostfile_7 Pi 2147483647  >> benchmark.dat
-mpirun -np 8 --hostfile myhostfile_8 Pi 2147483647  >> benchmark.dat
+mpirun -np 1  --hostfile myhostfile_1 pi 2147483647 >> benchmark.dat
+mpirun -np 2  --hostfile myhostfile_2 pi 2147483647 >> benchmark.dat
+mpirun -np 3  --hostfile myhostfile_3 pi 2147483647 >> benchmark.dat
+mpirun -np 4  --hostfile myhostfile_4 pi 2147483647 >> benchmark.dat
+mpirun -np 5 --hostfile myhostfile_5 pi 2147483647  >> benchmark.dat
+mpirun -np 6 --hostfile myhostfile_6 pi 2147483647  >> benchmark.dat
+mpirun -np 7 --hostfile myhostfile_7 pi 2147483647  >> benchmark.dat
+mpirun -np 8 --hostfile myhostfile_8 pi 2147483647  >> benchmark.dat
 
 
 echo "Weak Scaling"
 echo "Weak Scaling" >> benchmark.dat
 
-mpirun -np 1  --hostfile myhostfile_1 Pi 100000000  >> benchmark.dat
-mpirun -np 2  --hostfile myhostfile_2 Pi 200000000  >> benchmark.dat
-mpirun -np 3  --hostfile myhostfile_3 Pi 300000000  >> benchmark.dat
-mpirun -np 4  --hostfile myhostfile_4 Pi 400000000  >> benchmark.dat
-mpirun -np 5 --hostfile myhostfile_5 Pi 500000000  >> benchmark.dat
-mpirun -np 6 --hostfile myhostfile_6 Pi 600000000  >> benchmark.dat
-mpirun -np 7 --hostfile myhostfile_7 Pi 700000000  >> benchmark.dat
-mpirun -np 8 --hostfile myhostfile_8 Pi 800000000  >> benchmark.dat
+mpirun -np 1  --hostfile myhostfile_1 pi 100000000  >> benchmark.dat
+mpirun -np 2  --hostfile myhostfile_2 pi 200000000  >> benchmark.dat
+mpirun -np 3  --hostfile myhostfile_3 pi 300000000  >> benchmark.dat
+mpirun -np 4  --hostfile myhostfile_4 pi 400000000  >> benchmark.dat
+mpirun -np 5 --hostfile myhostfile_5 pi 500000000  >> benchmark.dat
+mpirun -np 6 --hostfile myhostfile_6 pi 600000000  >> benchmark.dat
+mpirun -np 7 --hostfile myhostfile_7 pi 700000000  >> benchmark.dat
+mpirun -np 8 --hostfile myhostfile_8 pi 800000000  >> benchmark.dat
 
 echo "----------------------" >> benchmark.dat
